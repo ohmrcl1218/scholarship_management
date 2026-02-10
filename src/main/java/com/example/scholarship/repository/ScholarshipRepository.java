f37df0cd38bb523c55bde687c23c62255ca555be
Mercel Macasinag <macasinag2611@gmail.com>
2026-02-10 10:56:11 +0800

    deleting model,controller, and repo for scholarship

--- src/main/java/com/example/scholarship/repository/ScholarshipRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.scholarship.repository;
-
-import com.example.scholarship.model.Scholarship;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScholarshipRepository extends JpaRepository<Scholarship, Long> {
-}
